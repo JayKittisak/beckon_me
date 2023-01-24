04ee7864e21ce9abb0596f052affbc022e5077cd
JayKittisak <homosapien_jay@hotmail.com>
2023-01-24 12:54:05 +0700

    Update index.js
    test

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/test', (req, res) => {
+    console.log("Just got a test request!")
+    res.send('Test!')
+})
+
 app.listen(process.env.PORT || 3000)
