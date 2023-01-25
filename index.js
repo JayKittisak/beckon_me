2dca6fbc78451310f81f1ebe8c1417959f50ea07
jayKittisak@mac <kittisak@madtech.co.th>
2023-01-25 15:41:16 +0700

    initial

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
+require('dotenv').config()
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+const port = process.env.PORT || 3000
+const { LINE_ACCESS_TOKEN } = process.env
+
+console.log(LINE_ACCESS_TOKEN);
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 app.all('/test', (req, res) => {
-    console.log("Just got a test request!")
-    res.send('Test!')
+    reply(reply_token)
+    res.sendStatus(200)
 })
 
-app.listen(process.env.PORT || 3000)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {'+LINE_ACCESS_TOKEN+'}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
