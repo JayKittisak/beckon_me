f7dea4028228d9f192689712c6861bfc44fb84d3
jayKittisak@mac <kittisak@madtech.co.th>
2023-01-29 14:48:40 +0700

    debug

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.all('/beckon', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-     console.log(req.body);
+    console.log(req.body);
+    console.log(req.body.source);
     let reply_token = req.body.events[0].replyToken
     
     massge = [
